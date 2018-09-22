8e2bb5d008e5d7b162b6ccec1c2f93cbf44d262b
denlly <fan91163@gmail.com>
2018-09-22 17:09:40 +0800

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = 'JupyterLab(beta)'
+project = 'JupyterLab'
 copyright = '2018, Project Jupyter'
 author = 'Project Jupyter'
 
@@ -129,7 +129,7 @@ html_sidebars = {
 
 # Output for github to be used in links
 html_context = {
-    "display_github": False,  # Integrate GitHub
+    "display_github": True,  # Integrate GitHub
     "github_user": "denlly",  # Username
     "github_repo": "jupyter-doc",  # Repo name
     "github_version": "master",  # Version
